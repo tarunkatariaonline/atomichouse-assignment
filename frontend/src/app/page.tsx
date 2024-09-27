d97114dda87b1d61c2fc0334583598f89b0a4b97
tarunkatariaonline <tarunsoftsol@gmail.com>
2024-09-27 15:46:20 +0530

    first

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,9 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 
 import { useCallback } from "react";
-import Logo from '@/Assets/clickup-symbol-logo-BB24230BBB-seeklogo.com.png'
+import Logo from "@/Assets/clickup-symbol-logo-BB24230BBB-seeklogo.com.png";
 import { MdDelete, MdEdit } from "react-icons/md";
 
 import { useGetAllTasks } from "@/hooks/getTask";
@@ -13,137 +13,139 @@ import { useTaskDelete } from "@/hooks/taskDelete";
 import { useUpdateTask } from "@/hooks/taskUpdate";
 import CreateTask from "./Components/ClientComponents/CreateTask";
 
-
-
 export default function Home() {
+  const { data = [], isLoading, isError } = useGetAllTasks();
+  const { mutate } = useTaskDelete();
+  const { mutate: mutateUpdate } = useUpdateTask();
 
+  //  console.log(data)
+  const updateStatusHandler = useCallback((id: string, status: string) => {
+    let newStatus;
+    if (status === "TODO") {
+      newStatus = "INPROGRESS";
+    } else if (status === "INPROGRESS") {
+      newStatus = "DONE";
+    } else {
+      newStatus = "TODO";
+    }
+    mutateUpdate({
+      id,
+      status: newStatus,
+    });
+  }, []);
 
- const {data=[],isLoading,isError}= useGetAllTasks()
- const {mutate} = useTaskDelete()
- const {mutate : mutateUpdate} = useUpdateTask()
- 
- 
-//  console.log(data)
-const updateStatusHandler = useCallback((id:string,status:string)=>{
+  if (isLoading) {
+    return (
+      <div>
+        <div className=" w-full  min-h-[100vh] flex justify-center items-center bg-gray-200">
+          <div className=" w-[40%]  max-md:w-[90%] shadow-xl  bg-white  min-h-0 rounded-md  p-2 ">
+            <div className=" w-full flex justify-center m-2">
+              <Image width={25} height={25} src={Logo} alt="logo" />
+            </div>
 
-  let newStatus;
-if(status==="TODO"){
-  newStatus="INPROGRESS"
+            <div className=" w-full flex justify-center font-semibold">
+              Loading....
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
+  if (isError) {
+    return (
+      <div>
+        <div className=" w-full  min-h-[100vh] flex justify-center items-center bg-gray-200">
+          <div className=" w-[40%]  max-md:w-[90%] shadow-xl  bg-white  min-h-0 rounded-md  p-2 ">
+            <div className=" w-full flex justify-center m-2">
+              <Image width={25} height={25} src={Logo} alt="logo" />
+            </div>
 
-}else if (status==="INPROGRESS"){
-  newStatus="DONE"
-}else{
-  newStatus="TODO"
-}
-mutateUpdate({id,
-  status:newStatus
-})
+            <div className=" w-full flex justify-center font-semibold">
+              Error in api fetch !
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-},[])
-
-
-
- if(isLoading){
-  return <div>
-<div className=' w-full  min-h-[100vh] flex justify-center items-center bg-gray-200'>
-   <div className=' w-[40%]  max-md:w-[90%] shadow-xl  bg-white  min-h-0 rounded-md  p-2 '>
-
-<div className=' w-full flex justify-center m-2'>
-<Image width={25} height={25} src={Logo} alt='logo'/>   
-</div>  
-     
-     <div className=" w-full flex justify-center font-semibold">
- Loading.... 
- </div> 
-     </div>
-       </div>
-
-</div>
- }
-
- if(isError){
-  return <div>
-<div className=' w-full  min-h-[100vh] flex justify-center items-center bg-gray-200'>
-   <div className=' w-[40%]  max-md:w-[90%] shadow-xl  bg-white  min-h-0 rounded-md  p-2 '>
-
-<div className=' w-full flex justify-center m-2'>
-<Image width={25} height={25} src={Logo} alt='logo'/>   
-</div>  
-     
-     <div className=" w-full flex justify-center font-semibold">
-    Error in api fetch !
- </div> 
-     </div>
-       </div>
-
-</div>
- }
- 
-
-
- const taskDeleteHandler  = (id:string)=>{
-  mutate({id})
- }
+  const taskDeleteHandler = (id: string) => {
+    mutate({ id });
+  };
 
   return (
-<>
+    <>
+      <div>
+        <div className=" w-full  min-h-[100vh] flex flex-col justify-center items-center bg-gray-200">
+          <div className=" w-[40%]  max-md:w-[90%] shadow-xl  bg-white  min-h-0 rounded-md  p-2 ">
+            <div className=" w-full flex justify-center m-2">
+              <Image width={25} height={25} src={Logo} alt="logo" />
+            </div>
 
-<div>
- 
-<div className=' w-full  min-h-[100vh] flex flex-col justify-center items-center bg-gray-200'>
- 
-   <div className=' w-[40%]  max-md:w-[90%] shadow-xl  bg-white  min-h-0 rounded-md  p-2 '>
+            <CreateTask />
+            {data.length === 0 && (
+              <div className=" w-full  flex justify-center flex-col items-center">
+                <p className=" font-semibold">No Task Found !</p>
+               
+              </div>
+            )}
 
-<div className=' w-full flex justify-center m-2'>
-<Image width={25} height={25} src={Logo} alt='logo'/>   
-</div>  
-
-
-<CreateTask/>
-{data.length===0&&<div className=" w-full  flex justify-center flex-col items-center">
-   <p className=" font-semibold">No Task Found !</p>
-  <Link href={'/task/create'}> <button className=" bg-blue-500 h-6 my-2 hover:bg-blue-700 text-white font-bold py-
-   4 px-4 rounded">Go To Create Task Page</button></Link>
-</div>
-}
-
-
-{data.map((task)=>{
-  return <div key={task.id} className=" w-full p-2  my-2 min-h-8 flex items-center justify-between border-gray-100 border-2 rounded-md">
-
-  <div className="  w-[90%]  ">
-    <p className=" font-medium">{task.title}</p>
-    {(task.description?.length!==0)&&<p>{task.description}
-       </p>}
-  </div>
-  <div className=" flex items-center h-full">
-    <button onClick={()=>{
-      updateStatusHandler(task.id,task.status)
-    }} className={` text-sm px-2 ${task.status==="TODO"?"bg-gray-600":(task.status==="DONE")?"bg-green-500":"bg-blue-500"}  ${task.status==="TODO"?"border-gray-700":(task.status==="DONE")?"border-green-700":"border-blue-700"} border-2 text-white rounded-md`}>
-    <p>{task.status}</p>
-    </button>
-    <Link href={`/task/edit/${task.id}`}> <button className=" p-1 bg-blue-500 hover:bg-blue-700 rounded-md mx-1 ">
- <MdEdit  size={18} color="white"/>
-    </button>
-    </Link> 
-    <button className=" p-1 bg-red-500 hover:bg-red-700 rounded-md mx-1 ">
-      <MdDelete onClick={()=>{
-       taskDeleteHandler(task.id)
-      }}  size={18} color="white"/>
-    </button>
-    
-  </div>
-  
-  </div>
-})}
-
-     
-   
-     </div>
-       </div>
-
-</div>
-</>
+            {data.map((task) => {
+              return (
+                <div
+                  key={task.id}
+                  className=" w-full p-2  my-2 min-h-8 flex items-center justify-between border-gray-100 border-2 rounded-md"
+                >
+                  <div className="  w-[90%]  ">
+                    <p className=" font-medium">{task.title}</p>
+                    {task.description?.length !== 0 && (
+                      <p>{task.description}</p>
+                    )}
+                  </div>
+                  <div className=" flex items-center h-full">
+                    <button
+                      onClick={() => {
+                        updateStatusHandler(task.id, task.status);
+                      }}
+                      className={` text-sm px-2 ${
+                        task.status === "TODO"
+                          ? "bg-gray-600"
+                          : task.status === "DONE"
+                          ? "bg-green-500"
+                          : "bg-blue-500"
+                      }  ${
+                        task.status === "TODO"
+                          ? "border-gray-700"
+                          : task.status === "DONE"
+                          ? "border-green-700"
+                          : "border-blue-700"
+                      } border-2 text-white rounded-md`}
+                    >
+                      <p>{task.status}</p>
+                    </button>
+                    <Link href={`/task/edit/${task.id}`}>
+                      {" "}
+                      <button className=" p-1 bg-blue-500 hover:bg-blue-700 rounded-md mx-1 ">
+                        <MdEdit size={18} color="white" />
+                      </button>
+                    </Link>
+                    <button className=" p-1 bg-red-500 hover:bg-red-700 rounded-md mx-1 ">
+                      <MdDelete
+                        onClick={() => {
+                          taskDeleteHandler(task.id);
+                        }}
+                        size={18}
+                        color="white"
+                      />
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
--- frontend/src/app/task/edit/[id]/page.tsx
+++ frontend/src/app/task/edit/[id]/page.tsx
@@ -8,56 +8,51 @@ import Logo from '@/Assets/clickup-symbol-logo-BB24230BBB-seeklogo.com.png'
 import { useGetSingleTask } from '@/hooks/getTask'
 
 import { useUpdateTask } from '@/hooks/taskUpdate'
-import { useState,useCallback,useEffect } from 'react'
+import { useState, useCallback, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import toast from 'react-hot-toast'
 
 
 const Page = ({ params }: { params: { id: string } }) => {
-   const {id} = params
-   const router = useRouter()
-   const [status,setStatus] = useState<string>("TODO")
-    const [title,setTitle] = useState<string>("")
-    const [description,setDescription] = useState<string|undefined>("")
-    const {data,isLoading,isError}= useGetSingleTask(id)
-     const {mutate}= useUpdateTask()
-    useEffect(()=>{
-        if(data){
+    const { id } = params
+    const router = useRouter()
+    const [status, setStatus] = useState<string>("TODO")
+    const [title, setTitle] = useState<string>("")
+    const [description, setDescription] = useState<string | undefined>("")
+    const { data, isLoading, isError } = useGetSingleTask(id)
+    const { mutate } = useUpdateTask()
+    useEffect(() => {
+        if (data) {
             setStatus(data.status)
             setTitle(data.title)
             setDescription(data.description)
         }
 
-    },[data])
-    const updateTaskHandler = useCallback(()=>{
+    }, [data])
+    const updateTaskHandler = useCallback(() => {
 
         // console.log("updateTask")
 
-        if(title.length===0){
+        if (title.length === 0) {
             return toast.error("Please Enter The Title !")
         }
-       
-         mutate({
-           id,
+
+        mutate({
+            id,
             title,
             description,
             status
-         })
+        })
 
-         router.push('/')
-    },[mutate,id,title,description,status])
+        router.push('/')
+    }, [mutate, id, title, description, status])
 
 
     if (isLoading) return <div>Loading...</div>
     if (isError) return <div>You Gave Wrong Id !</div>
-    if(data===null){
+    if (data === null) {
         return <div>Wrong Id of Task !</div>
     }
-
-
-   
-  
-
 
     return (
         <div className=' w-full  min-h-[100vh] flex justify-center items-center bg-gray-200'>
@@ -66,42 +61,42 @@ const Page = ({ params }: { params: { id: string } }) => {
                 <div className=' w-full flex justify-center m-2'>
                     <Image width={25} height={25} src={Logo} alt='logo' />
                 </div>
-              
-                <form onSubmit={(e) => {
-        e.preventDefault()
-        updateTaskHandler()
 
-    }} className=' w-full  px-3 h-full  ' >
-        <p >Task Title :</p>
-        <input value={title} onChange={(e)=>{
-            setTitle(e.target.value)
-        }} type="text" className=' w-full h-10 border-gray-500 outline-blue-400  my-2 rounded-md  border-2
+                <form onSubmit={(e) => {
+                    e.preventDefault()
+                    updateTaskHandler()
+
+                }} className=' w-full  px-3 h-full  ' >
+                    <p >Task Title :</p>
+                    <input value={title} onChange={(e) => {
+                        setTitle(e.target.value)
+                    }} type="text" className=' w-full h-10 border-gray-500 outline-blue-400  my-2 rounded-md  border-2
 ' placeholder=' Enter Task Title' />
 
 
 
-        <p >Task Description :</p>
-        <textarea value={description} onChange={(e)=>{
-            setDescription(e.target.value)
+                    <p >Task Description :</p>
+                    <textarea value={description} onChange={(e) => {
+                        setDescription(e.target.value)
 
-        }}  className=' w-full h-20 border-2 border-gray-500 outline-blue-400  my-2 rounded-md
+                    }} className=' w-full h-20 border-2 border-gray-500 outline-blue-400  my-2 rounded-md
 ' placeholder=' Enter Task Description' />
 
-        <p >Task Status : {data?.status}</p>
-        <select className=' w-full h-10 border-2 border-gray-500 outline-blue-400  my-2 rounded-md
-' value={status} onChange={(e)=>{
-    setStatus(e.target.value)
-}}>
-            <option value="TODO">TODO</option>
-            <option value="INPROGRESS">INPROGRESS</option>
-            <option value="DONE">DONE</option>
+                    <p >Task Status : {data?.status}</p>
+                    <select className=' w-full h-10 border-2 border-gray-500 outline-blue-400  my-2 rounded-md
+' value={status} onChange={(e) => {
+                            setStatus(e.target.value)
+                        }}>
+                        <option value="TODO">TODO</option>
+                        <option value="INPROGRESS">INPROGRESS</option>
+                        <option value="DONE">DONE</option>
 
-        </select>
-        <button type='submit' className=' text-white bg-gradient-to-r from-pink-400 via-blue-500 to-yellow-300  my-4 w-full h-10 font-bold  hover:from-pink-500 hover:via-blue-600 hover:to-yellow-500 rounded-md'>
-            UPDATE TASK
-        </button>
+                    </select>
+                    <button type='submit' className=' text-white bg-gradient-to-r from-pink-400 via-blue-500 to-yellow-300  my-4 w-full h-10 font-bold  hover:from-pink-500 hover:via-blue-600 hover:to-yellow-500 rounded-md'>
+                        UPDATE TASK
+                    </button>
 
-    </form>
+                </form>
 
             </div>
         </div>
